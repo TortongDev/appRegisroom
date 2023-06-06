84d16cffc5c13bf183a3a21280ea12b510688005
tortongDev <kittithat.charoenkhet101@gmail.com>
2023-06-05 20:54:57 -0700

    edit  include cdn

--- autoload_cdn.php
+++ autoload_cdn.php
@@ -3,16 +3,19 @@
     function autoload_cdn(){
         $pathAll = array(
             "./node_modules/bootstrap/dist/css",
+            "./public",
             "./public/css",
             "./public/js",
+            "../public",
             "../public/css",
+            "../../public",
             "../public/js",
             "../../public/css",
-            "../../public/js",
+            "../../public/js"
         );   
         foreach ($pathAll as $key => $value) {
-            if(file_exists($value)):
-                $path = "{$value}/cdn.php";
+            $path = "{$value}/cdn.php";
+            if(file_exists($path)):
                 require_once "{$path}";
             endif;
         }
--- pageView/index.php
+++ pageView/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php include('autoload_cdn.php'); ?>
+    <?php include('../autoload_cdn.php'); ?>
     <title>My Home</title>
 </head>
 <body>
