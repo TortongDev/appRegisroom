e027afd952ce87bdd05746ff0892702bb0ac342b
TortongDev <kittithat.charoenkhet101@gmail.com>
2023-06-10 15:29:37 +0700

    remove  packet json

--- public/cdn.php
+++ public/cdn.php
@@ -4,3 +4,9 @@
 <link rel="stylesheet" href="/appRegisroom/public/css/footer.css"> -->
 <link rel="stylesheet" href="/appRegisroom/node_modules/jquery/dist/jquery.min.js">
 <!-- <script src="/appRegisroom/public/js/script.js"></script> -->
+
+<script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://unpkg.com/vue-router@4.0.15/dist/vue-router.global.js"></script>
